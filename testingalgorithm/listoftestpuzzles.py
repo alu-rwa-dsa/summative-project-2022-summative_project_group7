934894e9ceae11738b8ae6b6eb1e3e6229a897bc
johnthuo1 <90395290+johnthuo1@users.noreply.github.com>
2022-04-14 15:09:48 +0300

    First Commit

--- testingalgorithm/listoftestpuzzles.py
+++ testingalgorithm/listoftestpuzzles.py
@@ -1,5 +1,5 @@
 listOfPuzzles = [
-     # Puzzle has 2 empty cells
+    # Puzzle has 2 empty cells
     [[5, 3, 4, 0, 7, 8, 9, 1, 2],
      [6, 7, 2, 1, 9, 5, 3, 4, 8],
      [1, 9, 8, 3, 4, 2, 5, 6, 7],
@@ -10,7 +10,7 @@ listOfPuzzles = [
      [2, 8, 7, 4, 1, 9, 6, 3, 5],
      [3, 4, 5, 2, 8, 6, 1, 7, 9]],
 
-    # Puzzle has 4 empty spaces
+    # Puzzle has 4 empty cells
     [[1, 3, 2, 6, 5, 8, 4, 7, 9],
      [5, 9, 8, 7, 4, 3, 1, 6, 2],
      [7, 6, 4, 2, 9, 1, 5, 3, 8],
@@ -21,7 +21,7 @@ listOfPuzzles = [
      [9, 1, 5, 3, 0, 4, 6, 8, 7],
      [6, 8, 3, 1, 7, 9, 2, 5, 0]],
 
-    # Puzzle Has 6 empty spaces
+    # Puzzle Has 6 empty cells
     [[7, 8, 2, 6, 9, 5, 3, 1, 4],
      [1, 4, 5, 2, 3, 8, 7, 6, 9],
      [9, 3, 0, 0, 4, 0, 5, 8, 2],
@@ -32,7 +32,7 @@ listOfPuzzles = [
      [2, 7, 8, 3, 6, 4, 1, 9, 5],
      [5, 1, 3, 0, 7, 2, 8, 4, 6]],
 
-    # Puzzle has 8 empty Spaces
+    # Puzzle has 8 empty cells
     [[8, 2, 9, 0, 1, 7, 3, 4, 6],
      [4, 1, 6, 2, 3, 8, 9, 5, 0],
      [5, 3, 7, 9, 4, 6, 2, 1, 8],
@@ -43,7 +43,7 @@ listOfPuzzles = [
      [2, 9, 8, 3, 5, 4, 7, 6, 0],
      [1, 7, 4, 8, 6, 9, 5, 2, 3]],
 
-    # Puzzle has 10 Spaces
+    # Puzzle has 10 empty cells
     [[8, 2, 3, 4, 9, 6, 1, 5, 0],
      [7, 5, 0, 2, 1, 8, 3, 9, 6],
      [1, 6, 9, 0, 5, 3, 2, 8, 0],
@@ -54,7 +54,7 @@ listOfPuzzles = [
      [3, 8, 7, 9, 6, 2, 4, 1, 5],
      [5, 1, 6, 3, 7, 4, 9, 2, 8]],
 
-    # Puzzle has  12 spaces
+    # Puzzle has  12 empty cells
     [[1, 9, 8, 6, 7, 2, 3, 0, 0],
      [3, 4, 7, 9, 1, 5, 8, 0, 2],
      [5, 0, 0, 0, 0, 3, 9, 7, 1],
@@ -65,7 +65,7 @@ listOfPuzzles = [
      [4, 6, 2, 5, 9, 1, 7, 3, 8],
      [8, 3, 1, 2, 4, 7, 6, 5, 9]],
 
-    # Puzzle has 14 spaces
+    # Puzzle has 14 empty cells
     [[5, 4, 6, 7, 0, 2, 0, 1, 9],
      [9, 7, 2, 3, 5, 1, 0, 8, 6],
      [3, 1, 0, 0, 9, 4, 7, 0, 0],
@@ -76,7 +76,7 @@ listOfPuzzles = [
      [4, 2, 7, 1, 6, 9, 5, 3, 8],
      [1, 6, 5, 8, 4, 3, 9, 7, 2]],
 
-    # Puzzle has 16 spaces
+    # Puzzle has 16 empty cells
     [[6, 7, 0, 0, 0, 9, 0, 4, 0],
      [2, 9, 3, 1, 4, 5, 7, 8, 6],
      [5, 8, 0, 2, 7, 6, 1, 3, 0],
@@ -87,7 +87,7 @@ listOfPuzzles = [
      [7, 3, 5, 9, 2, 4, 8, 0, 1],
      [9, 6, 8, 7, 5, 1, 4, 0, 3]],
 
-    # Puzzle has 18 empty spaces
+    # Puzzle has 18 empty cells
     [[0, 0, 1, 7, 4, 2, 5, 0, 0],
      [0, 6, 2, 1, 8, 3, 9, 7, 4],
      [0, 3, 0, 5, 0, 6, 1, 8, 2],
@@ -98,7 +98,7 @@ listOfPuzzles = [
      [6, 1, 5, 8, 3, 4, 2, 0, 7],
      [9, 7, 8, 6, 2, 1, 4, 0, 5]],
 
-    # Puzzle has 20 empty spaces
+    # Puzzle has 20 empty cells
     [[1, 3, 8, 0, 4, 0, 0, 7, 0],
      [9, 4, 5, 0, 6, 2, 8, 3, 1],
      [2, 6, 7, 3, 1, 8, 9, 0, 5],
@@ -109,7 +109,7 @@ listOfPuzzles = [
      [4, 1, 6, 0, 5, 9, 0, 0, 0],
      [7, 8, 2, 0, 3, 0, 6, 5, 9]],
 
-    # Puzzle has 22 empty spaces
+    # Puzzle has 22 empty cells
     [[8, 0, 5, 3, 0, 1, 2, 0, 7],
      [2, 4, 6, 7, 9, 8, 1, 5, 3],
      [0, 3, 7, 2, 5, 4, 6, 0, 0],
@@ -120,7 +120,7 @@ listOfPuzzles = [
      [3, 0, 2, 4, 7, 5, 0, 1, 6],
      [5, 0, 1, 9, 0, 0, 8, 0, 0]],
 
-    # Puzzle has 24 empty spaces
+    # Puzzle has 24 empty cells
     [[5, 6, 8, 9, 1, 0, 0, 2, 7],
      [3, 4, 2, 6, 8, 0, 0, 1, 0],
      [1, 9, 7, 2, 5, 4, 0, 8, 3],
@@ -131,7 +131,7 @@ listOfPuzzles = [
      [8, 5, 1, 7, 2, 6, 3, 4, 9],
      [4, 7, 3, 0, 0, 0, 0, 5, 6]],
 
-    # Puzzle has 26 empty spaces
+    # Puzzle has 26 empty cells
     [[4, 1, 5, 7, 3, 8, 2, 6, 0],
      [9, 2, 8, 6, 5, 1, 0, 0, 3],
      [7, 6, 3, 2, 4, 9, 5, 0, 1],
@@ -143,8 +143,3 @@ listOfPuzzles = [
      [8, 3, 1, 0, 0, 0, 0, 4, 0]]
 ]
 
-# print(len(listOfPuzzles))
-# for i in range(len(listOfPuzzles)):
-#     puzzle = listOfPuzzles[i]
-#     if i == 1:
-#         print(puzzle)
